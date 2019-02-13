51f7026f169897035bc2705608c1fb99221f5514
Gustavo Gutierrez <gustavo@acidtango.com>
2019-02-13 11:59:23 +0100

    Adapted the infrastructure to use SwarmInit Compose

--- load_balancer.tf
+++ load_balancer.tf
@@ -32,6 +32,7 @@ resource "aws_lb_target_group" "ec2_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${aws_vpc.main.id}"
+  deregistration_delay = 30
 }
 
 resource "aws_lb_listener" "port_80_listener" {
@@ -68,4 +69,3 @@ resource "aws_lb_listener" "port_80_listener" {
 #     target_group_arn = "${aws_lb_target_group.ec2_tg.arn}"
 #   }
 # }
-
